71574d87641d9a67cca5db79a31625c54b6ad560
sara <saraiblo2021@gmail.com>
2024-10-18 18:38:26 -0600

    crear custom hooks

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,52 +1,15 @@
-import { useEffect, useState } from "react";
 import "./App.css"
-import { getRandomFact } from "./services/facts";
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-//const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true'
-const CAT_PREFIX_IMAGE_URL = `https://cataas.com`
+import { useCatImage } from "./hooks/useCatImage";
+import { useCatFat } from "./hooks/useCatFact";
+import { Otro } from "./Components/Otro";
 
 export function App() {
-    const [fact, setFact] = useState()
-    const [imageUrl, SetImageUrl] = useState()
-
-    //para recuperar la cita al cargar la pagina
-    useEffect(() => {
-        getRandomFact().then(setFact)
-    }, [])
-
-     
-    //para recuperar la imagen cada vez que tenemos una cita nueva
-    useEffect(() => {
-        if(!fact) return
-        //split separar cadena de texto
-        //slice obtener las tres primeras palabras
-        //join unir las palabras en una cadena de texto
-        //const firstWord = fact.split(' ').slice(0, 3).join(' ')
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-        console.log(threeFirstWords)
-
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-        .then(res => res.json()) //respuesta 
-        .then(response => {
-            const {url} = response
-            SetImageUrl(url)
-        })
-    }, [fact])
-
-    /*Ejemplo de async await
-        useEffect(() => {
-        async function getRandomFact(){
-            const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
-            const json = await res.json()setFact(json.fact)
-        }
-        
-        getRandomFact()
-    }, [])*/
+    
+    const {fact, refreshFact} = useCatFat()
+    const {imageUrl} = useCatImage({fact})
 
     const handleClick = async () => {
-        const newFact = await getRandomFact()
-        setFact(newFact)
+        refreshFact()
     }
 
     return (
@@ -55,7 +18,9 @@ export function App() {
             <button onClick={handleClick}>Get new fact</button>
 
             {fact && <p>{fact}</p>} //renderizado condicional
-            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+
+            <Otro />
         </main>        
     )
 }
---
+++ projects/04-react-prueba-tecnica/src/hooks/useCatImage.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export function useCatImage ({fact}) {
+    const [imageUrl, SetImageUrl] = useState()
+
+    //para recuperar la imagen cada vez que tenemos una cita nueva
+    useEffect(() => {
+        if(!fact) return
+        //split separar cadena de texto
+        //slice obtener las tres primeras palabras
+        //join unir las palabras en una cadena de texto
+        //const firstWord = fact.split(' ').slice(0, 3).join(' ')
+        const threeFirstWords = fact.split(' ', 3).join(' ')        
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+        .then(res => res.json()) //respuesta 
+        .then(response => {
+            const {url} = response
+            SetImageUrl(url)
+        })
+    }, [fact])
+
+    //Guardar y recuperar la imagen
+    return {imageUrl: `${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+} //{ imageUrl: 'https://...'}
