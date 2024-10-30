a3c4b8e4ea1c4b9bc4e2300c338ca3cf6eac17ac
Sarai <sarai.ilopez@roatech.com.mx>
2024-10-30 13:50:09 -0600

    actualizacion del proyecto

--- projects/06-shopping-cart/src/reducers/cart.js
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -1,4 +1,4 @@
-export const cartInitialState = []
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || []
 
 export const CART_ACTION_TYPES = {
     ADD_TO_CART: 'ADD_TO_CART',
@@ -6,7 +6,59 @@ export const CART_ACTION_TYPES = {
     CLEAR_CART: 'CLEAR_CART'
 }
 
+// update localStorage with state for cart
+export const updateLocalStorage = state => {
+    window.localStorage.setItem('cart', JSON.stringify(state))
+}
 
+const UPDATE_STATE_BY_ACTION = {
+    [CART_ACTION_TYPES.ADD_TO_CART]: (state, action) => {
+        const {id} = action.Payload
+        const productInCartIndex = state.findIndex(item =>item.id === id)
+
+        if(productInCartIndex >= 0) {
+            const newState = [
+                ...state.slice(0, productInCartIndex),
+                { ...state[productInCartIndex], quantity: state[productInCartIndex].quantity + 1 },
+                ...state.slice(productInCartIndex + 1)
+            ]
+
+            updateLocalStorage(newState)
+            return newState 
+        }
+
+        const newState = [
+            ...state,
+            {
+                ...action.payload,
+                quantity: 1
+            }
+        ]
+
+        updateLocalStorage(newState)
+        return newState 
+    },
+
+    [CART_ACTION_TYPES.REMOVE_FROM_CART]: (state, action) => {
+        const {id} = action.payload
+        const newState = state.filter(item => item.id !== id)
+        updateLocalStorage(newState)
+        return newState
+    },
+
+    [CART_ACTION_TYPES.CLEAR_CART]: () => {
+        updateLocalStorage([])
+        return []
+    }
+}
+
+export const cartReducer = (state, action) => {
+    const { type: actionType } = action
+    const updateState = UPDATE_STATE_BY_ACTION[actionType]
+    return updateState ? updateState(state, action) : state
+}
+
+/*
 export const cartReducer = (state, action) => {
     const {type: actionType, payload: actionPayload} = action
     switch (actionType){
@@ -18,27 +70,36 @@ export const cartReducer = (state, action) => {
                 //una forma de crear el carrito seria usando structuredClone
                 const newState = structuredClone(state)
                 newState[productInCartIndex].quantity += 1
+                updateLocalStorage(newState)
                 return newState
             }
 
-            return [
+            const newState = [
                 ...state,
                 {
                     ...actionPayload, //product
                     quantity: 1
                 }
             ]
+
+            updateLocalStorage(newState)
+            return newState 
         }
 
         case CART_ACTION_TYPES.REMOVE_FROM_CART: {
             const {id} = actionPayload
-            return state.filter(item => item.id !== id)
+            const newState = state.filter(item => item.id !== id)
+            updateLocalStorage(newState)
+            return newState
         }
 
-        case CART_ACTION_TYPES.CLEAR_CART: {
-            return  cartInitialState
+        case CART_ACTION_TYPES.CLEAR_CART: () => {
+            updateLocalStorage([])
+            return []
         }
     }
 
     return state
-}
+}*/
+
+
