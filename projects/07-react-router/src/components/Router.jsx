ae26a84140f3321e92b56e719f7795ce80dda78e
sara <saraiblo2021@gmail.com>
2024-11-01 17:48:13 -0600

    actualización del proyecto 7

--- projects/07-react-router/src/components/Router.jsx
+++ projects/07-react-router/src/components/Router.jsx
@@ -1,14 +1,15 @@
 import { EVENTS } from "../consts.js"
 import { useState, useEffect, Children } from "react"
 import { match } from "path-to-regexp"
+import { getCurrentPath } from "../utils.js"
 
 export function Router({children, routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1>}) {
     //tecnica del renderizado condicional
-    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+    const [currentPath, setCurrentPath] = useState(getCurrentPath())
   
     useEffect(() => {
       const onLocationChange = () => {
-        setCurrentPath(window.location.pathname)
+        setCurrentPath(getCurrentPath())
       }
   
       window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
